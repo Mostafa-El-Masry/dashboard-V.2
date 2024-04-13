c6f26cdae02567466fb40b98c996abee66a10983
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2024-04-13 23:53:35 +0300

    Adding First Todos And chck if remain there after deletion and reinstalling

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -23,7 +23,7 @@ export default async function Home() {
         <h1 className="text-2xl">Todos</h1>
         <Link
           className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
-          href="/new"
+          href="/todos/new"
         >
           New
         </Link>
